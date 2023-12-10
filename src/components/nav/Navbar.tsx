a1c547ec7c347cd9e72dd3173a83f008cd18d76b
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-12-10 18:34:47 +0700

    feat : Github OAuth

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import { SiGithub } from "react-icons/si";
+import LoginForm from "./LoginForm";
 
 export default function Navbar() {
   return (
@@ -12,10 +13,7 @@ export default function Navbar() {
         </Link>
         <div className="h-1 w-0 group-hover:w-full transition-all bg-violet-500"></div>
       </div>
-      <Button variant="outline" className="flex items-center gap-2">
-        <SiGithub />
-        Login
-      </Button>
+      <LoginForm />
     </nav>
   );
 }
