e50d9df08d8fedd999ec25564f93bcf8b8019ee8
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2024-01-04 15:47:46 +0700

    feat : Switch Form Edit

--- src/app/dashboard/components/BlogTable.tsx
+++ src/app/dashboard/components/BlogTable.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 import { EyeOpenIcon, Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import React from "react";
 import { Switch } from "@/components/ui/switch";
-import { readBlog } from "@/lib/actions/blog";
+import { readBlog, updateBlogById } from "@/lib/actions/blog";
 import DeleteAlert from "./DeleteAlert";
+import SwitchForm from "./SwitchForm";
+import { BlogFormSchemaType } from "../schema";
 
 export default async function BlogTable() {
   const { data: blogs } = await readBlog();
@@ -16,11 +18,26 @@ export default async function BlogTable() {
           <h1>Publish</h1>
         </div>
         {blogs?.map((blog, index) => {
+          const updatePremium = updateBlogById.bind(null, blog.id, {
+            is_premium: !blog.is_premium,
+          } as BlogFormSchemaType);
+          const updatePublish = updateBlogById.bind(null, blog.id, {
+            is_published: !blog.is_published,
+          } as BlogFormSchemaType);
+
           return (
             <div className="grid grid-cols-5 p-5" key={index}>
               <h1 className="col-span-2">{blog.title}</h1>
-              <Switch checked={blog.is_premium} />
-              <Switch checked={blog.is_published} />
+              <SwitchForm
+                checked={blog.is_premium}
+                name="premium"
+                onToggle={updatePremium}
+              />
+              <SwitchForm
+                checked={blog.is_published}
+                name="publish"
+                onToggle={updatePublish}
+              />
               <Actions id={blog.id} />
             </div>
           );
--- src/lib/actions/blog.ts
+++ src/lib/actions/blog.ts
@@ -51,3 +51,9 @@ export async function deleteBlogById(blogId: string) {
   revalidatePath(DASHBOARD);
   return JSON.stringify(result);
 }
+
+export async function updateBlogById(blogId: string, data: BlogFormSchemaType) {
+  const result = await supabase.from("blog").update(data).eq("id", blogId);
+  revalidatePath(DASHBOARD);
+  return JSON.stringify(result);
+}
